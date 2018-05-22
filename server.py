2abaa3646c66aa53120158a0f50ef1afd08e2502
hanalaydrus <hanalaydrus@gmail.com>
2018-05-22 09:51:11 +0700

    change logging tool

--- server.py
+++ server.py
@@ -28,6 +28,7 @@ import string
 import random
 import logging
 import grpc
+import numpy
 
 import semanticContract_pb2
 import semanticContract_pb2_grpc
@@ -36,17 +37,13 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 exitFlag = 0
 concurrent = 0
 
-
-
 def TimestampMillisec64():
     return int((datetime.utcnow() - datetime(1970, 1, 1)).total_seconds() * 1000)
 
 class Greeter(semanticContract_pb2_grpc.GreeterServicer):
     def SayHello(self, request, context):
         global concurrent
-
-        logging.basicConfig(filename='info.log',level=logging.INFO)
-        
+        log = []
         concurrent = concurrent + 1
         camera_id = request.id % 10
         random_id = str(request.id)
@@ -125,10 +122,12 @@ class Greeter(semanticContract_pb2_grpc.GreeterServicer):
                 else:
                     sentence = sentence + ("Terjadi penurunan volume kendaraan sebesar %d persen dibandingkan lalu lintas normal." % (volume_queue['percentage']))
             
-            logging.info("%s, %s, %s", random_id, TimestampMillisec64(), str(concurrent))
+            log.append([random_id, TimestampMillisec64(), str(concurrent)])
 
             yield semanticContract_pb2.HelloReply(response='%s' % sentence)
 
+        a = numpy.asarray(log)
+        numpy.savetxt("cc"+random_id+".csv", a, fmt="%s", delimiter=",")
         print("finish logging cc: %d" % concurrent)
         join_thread()
 
